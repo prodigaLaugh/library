1a491f13657f90014000ab068ffb86495c6b56ea
zhengcunjie <zhengcunjie@itangquan.com>
2020-07-08 09:45:13 +0800

    d

--- pkg/aler.umd.js
+++ pkg/aler.umd.js
@@ -776,43 +776,43 @@ var component = normalizeComponent(
 
 
 
-// aler.install = Vue => Vue.component("aler", aler);
+Alert.install = Vue => Vue.component("aler", Alert);
 
-// export default aler;
+/* harmony default export */ var components = (Alert);
 
 
  
-// 存储组件列表
-const components = [
-    Alert
-]
+// // 存储组件列表
+// const components = [
+//     Aler
+// ]
  
-// 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
-const install = function (Vue) {
+// // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
+// const install = function (Vue) {
   
-  // 判断是否安装
-  if (install.installed) return
-  // 遍历注册全局组件
-  components.map(component => { 
+//   // 判断是否安装
+//   if (install.installed) return
+//   // 遍历注册全局组件
+//   components.map(component => { 
     
-    Vue.component(component.name, component)})
-}
+//     Vue.component(component.name, component)})
+// }
 
-// 判断是否是直接引入文件
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
+// // 判断是否是直接引入文件
+// if (typeof window !== 'undefined' && window.Vue) {
+//   install(window.Vue)
+// }
  
-/* harmony default export */ var src_components = ({
-  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
-  install,
-  // 以下是具体的组件列表
-  aler: Alert
-});
+// export default {
+//   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
+//   install,
+//   // 以下是具体的组件列表
+//   Aler
+// }
 // CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/entry-lib.js
 
 
-/* harmony default export */ var entry_lib = __webpack_exports__["default"] = (src_components);
+/* harmony default export */ var entry_lib = __webpack_exports__["default"] = (components);
 
 
 
